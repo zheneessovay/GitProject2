3fb5f600e64c38e27125ecc85939d550a846cc61
Yerkezhan Zhenissova <zheneessovay@gmail.com>
2024-03-18 18:44:33 +0500

    Initial Commit

---
+++ GitProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitProject2
+//
+//  Created by Yerkezhan Zheneessova on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
